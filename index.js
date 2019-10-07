683e3c7ac31ea4aa644226846fd0aa03a79887ac
StXt <stas.domanskiy@gmail.com>
2019-10-07 13:30:22 +0300

    routes edited

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ const tasks = require('./payload/tasks/tasks');
 const users = require('./payload/users/users');
 const boards = require('./payload/boards/boards');
 
-app.get('/', (req, res) => res.send('Wellcome to Express\'ive api!'));
-
+app.get('/', (req, res) => res.send('Welcome to Express backend API!'));
+// USERS
 app.get('/users', users.getAll);
 app.get('/user/:id', users.getOne);
 app.post('/user', users.add);
 app.put('/user/:id', users.change);
 app.delete('/user/:id', users.delete);
-
+// BOARDS
 app.get('/boards', boards.getAll);
 app.get('/board/:id', boards.getOne);
 app.post('/board', boards.add);
 app.put('/board/:id', boards.change);
 app.delete('/board/:id', boards.delete);
-
+// LISTS
 app.get('/board/:boardId/lists', lists.getAll);
 app.get('/board/:boardId/list/:listId', lists.getOne);
 app.post('/board/:boardId/list', lists.add);
 app.put('/board/:boardId/list/:listId', lists.change);
 app.delete('/board/:boardId/list/:listId', lists.delete);
-
-app.get('/tasks', tasks.getAll);
-app.get('/task/:id', tasks.getOne);
-app.post('/task', tasks.add);
-app.put('/task/:id', tasks.change);
-app.delete('/task/:id', tasks.delete);
+// TASKS
+app.get('/board/:boardId/list/:listId/tasks', tasks.getAll);
+app.get('/board/:boardId/list/:listId/task/:id', tasks.getOne);
+app.post('/board/:boardId/list/:listId/task', tasks.add);
+app.put('/board/:boardId/list/:listId/task/:id', tasks.change);
+app.delete('/board/:boardId/list/:listId/task/:id', tasks.delete);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
