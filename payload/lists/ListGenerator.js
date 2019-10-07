cf1872f583d836456b22717983309ae064b0b2c7
StXt <stas.domanskiy@gmail.com>
2019-10-07 13:07:23 +0300

    task editting

--- payload/lists/ListGenerator.js
+++ payload/lists/ListGenerator.js
@@ -1,13 +1,10 @@
-const titlesArray = [
-  "Family Tasks", "Home Tasks", "Entertainment", "Job Tasks"
-];
-const ordersArray = [
-  "Important", "Desired", "Harebrained"
-];
+const titlesArray = require('./titlesArray');
+const ordersArray = require('./ordersArray');
 
 module.exports = (id) => {
   let randomTitle = Math.floor(Math.random() * titlesArray.length);
   let randomOrder = Math.floor(Math.random() * ordersArray.length);
+  
   return {
     "id": id,
     "title": titlesArray[randomTitle],
--- payload/tasks/TaskGenerator.js
+++ payload/tasks/TaskGenerator.js
@@ -1,15 +1,17 @@
-const verbs = [
-  "add", "allow", "bake", "bang", "call", "chase", "damage", "drop", "end","escape", "fasten", "fix", "gather", "grab", "hang", "hug", "imagine", "itch", "jog", "jump", "kick", "knit", "land", "lock", "march", "mix", "name", "notice", "obey", "open", "pass", "promise", "question", "reach", "rinse", "scatter", "stay", "talk","turn", "unite", "use", "vanish", "visit", "walk", "work", "yawn", "yell", "zip", "zoom"
-];
+const verbs = require('./verbs');
+const users = require('../users/usersData');
+const ordersArray = require('../lists/ordersArray');
 
 module.exports = (id) => {
-  
-  let randomIndex = Math.floor(Math.random() * verbs.length);
+  let randomVerb = Math.floor(Math.random() * verbs.length);
+  let randomOrder = Math.floor(Math.random() * users.length);
+  let randomUser = Math.floor(Math.random() * ordersArray.length);
+
   return {
     "id": id,
-    "title": `${verbs[randomIndex]} that stuff`,
-    "order": 1,
-    "assignee": "user",
+    "title": `${verbs[randomVerb]} that stuff`,
+    "order": ordersArray[randomOrder],
+    "assignee": users[randomUser][id],
     "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, officiis."
   }
 };
